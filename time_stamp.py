9d2a768459780efe86d99b5746cd6b7189cb5d26
abithpdm <abithpdm@gmail.com>
2020-07-12 21:12:02 +0530

    completed program

--- time_stamp.py
+++ time_stamp.py
@@ -2,3 +2,21 @@
 #distribution by hour of the day for each of the messages. You can pull the hour
 #out from the 'From ' line by finding the time and then splitting the string a
 #second time using a colon. 
+
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+time={}
+for lines in handle:
+    if lines.startswith("From "):
+        words=lines.split()
+        time_stamp=words[5].split(":")
+        if time_stamp[0] not in time:
+            time.update({time_stamp[0]:1})
+        else :
+            time[time_stamp[0]]=time.get(time_stamp[0],0)+1
+        
+sorteddict = sorted(time)
+for index in sorteddict :
+    print(index , time[index])
+        
