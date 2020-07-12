3d1f4c256bd477d8418691ae00b03ff1731a1bd2
abithpdm <abithpdm@gmail.com>
2020-07-12 20:24:53 +0530

    documentation update

--- email_print.py
+++ email_print.py
@@ -2,7 +2,9 @@
 # that starts with 'From ' like the following line: 
 #         From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
 
-
+#You will parse the From line using split() and print out the second word in 
+# the line (i.e. the entire address of the person who sent the message). 
+# Then print out a count at the end. 
 
 fname = input("Enter file name: ")
 if len(fname) < 1 : fname = "mbox-short.txt"
