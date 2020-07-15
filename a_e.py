4b0ecf7da346f5b1d52ea3b74ae42cc16ea83fac
abithpdm <abithpdm@gmail.com>
2020-07-15 17:39:38 +0530

    code

--- a_e.py
+++ a_e.py
@@ -1,1 +1,7 @@
 #Write code that counts the number of words in sentence that contain either an “a” or an “e”. Store the result in the variable num_a_or_e.
+sentence = "python is a high level general purpose programming language that can be applied to many different classes of problems."
+num_a_or_e=0
+words=sentence.split()
+for i in words:
+    if ("a" in i)or("e" in i):
+        num_a_or_e=num_a_or_e+1
