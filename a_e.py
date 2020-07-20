f9baadfa68dc691b4b3fc0c21a8ab891118210cb
abithpdm <abithpdm@gmail.com>
2020-07-20 21:01:13 +0530

    documentation

--- a_e.py
+++ a_e.py
@@ -1,5 +1,7 @@
 #Write code that counts the number of words in sentence that contain either an “a” or an “e”.
 #Store the result in the variable num_a_or_e.
+
+
 sentence = "python is a high level general purpose programming language that can be applied to many different classes of problems."
 num_a_or_e=0
 words=sentence.split()
