6ee01e403d5166dcf77db61daa7bb393da9dceac
abithpdm <abithpdm@gmail.com>
2020-07-22 22:19:29 +0530

    doc

--- fileread.py
+++ fileread.py
@@ -1,6 +1,6 @@
-# Write a program that prompts for a file name, then opens that file and reads through the file, 
+# Write a program that prompts for a file name, then opens that file and reads through the file,
 # and print the contents of the file in upper case.
-
+#we need to get the file name
 fname = input("Enter file name: ")
 fh = open(fname)
 y=fh.read()
