0b61e66a063da05bd5474049c201d87c6b440507
abithpdm <abithpdm@gmail.com>
2020-07-23 22:22:15 +0530

    doc

--- mail_sender.py
+++ mail_sender.py
@@ -1,10 +1,11 @@
-#Write a program to read through the mbox-short.txt and figure out 
-#who has sent the greatest number of mail messages. 
+#Write a program to read through the mbox-short.txt and figure out
+#who has sent the greatest number of mail messages.
 #The program looks for 'From ' lines and takes the second word of those lines as
-#the person who sent the mail. The program creates a Python dictionary that maps 
-#the sender's mail address to a count of the number of times they appear in the file. 
-#After the dictionary is produced, the program reads through the dictionary using 
+#the person who sent the mail. The program creates a Python dictionary that maps
+#the sender's mail address to a count of the number of times they appear in the file.
+#After the dictionary is produced, the program reads through the dictionary using
 #a maximum loop to find the most prolific committer.
+
 
 name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
@@ -17,13 +18,11 @@ for lines in handle:
             user.update({words[1]:1})
         else :
             user[words[1]]=user.get(words[1],0)+1
-maximum=0 
+maximum=0
 maximum_key=0
 for i in user :
     if user[i]>maximum:
         maximum=user[i]
         maximum_key=i
-        
-print(maximum_key ,user[maximum_key])
-    
 
+print(maximum_key ,user[maximum_key])
