c18d50de05f4656d835f1b379a09c445c64e0a06
akhil ps <49308257+akhilpsin@users.noreply.github.com>
2020-10-01 16:36:31 +0530

    updated with a function word_count(str)
    Modified the code by using a function

--- word_count.py
+++ word_count.py
@@ -1,11 +1,14 @@
-#The variable sentence stores a string. Write code to determine how many words in sentence start and end with the same letter, including one-letter words. Store the result in the variable same_letter_count.
+#function for counting each word is created
+def word_count(str):
+    counts = dict()
+    words = str.split() #here each letters is splited 
 
-sentence = "students flock to the arb for a variety of outdoor activities such as jogging and picnicking"
-words=sentence.split()
-same_letter_count=0
-# Write your code here.
-for i in words:
-    if i[0]==i[-1]:
-        same_letter_count=same_letter_count+1
-        
+    for word in words: #here in this 'for loop' until the words are over this loop continues
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
 
+    return counts
+
+print( word_count('the quick brown fox jumps over the lazy dog.'))
